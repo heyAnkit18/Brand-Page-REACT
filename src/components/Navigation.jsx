f618bc9683266da084452f9532537b25a79cf7fe
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-11-09 21:29:57 +0530

    Navigation Updated

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,39 @@
+import React, { useState } from "react";
+import "./Navigation.css";
+
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="container">
       <div className="logo">
         <img src="/images/brand_logo.png" alt="logo" />
       </div>
-      <ul>
-        <li href="#">Menu</li>
-        <li href="#">Location</li>
-        <li href="#">About</li>
-        <li href="#">Contact</li>
+
+      <ul className={`nav-links ${isOpen ? "active" : ""}`}>
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">Location</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
       </ul>
 
-      <button>login</button>
+      <button className="login-button">Login</button>
+
+      <div className="hamburger" onClick={toggleMenu}>
+        ☰
+      </div>
     </nav>
   );
 };
