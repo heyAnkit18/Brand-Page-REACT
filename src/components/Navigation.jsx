0855a8408ba6cc72b71558ce82c19e171f1925a2
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-11-09 21:24:54 +0530

    Navigation.jsx Added

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
 const Navigation = () => {
   return (
-    <div>Navigation</div>
-  )
-}
+    <nav className="container">
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="logo" />
+      </div>
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
 
-export default Navigation
+      <button>login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
